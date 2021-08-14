e42ca092a0d078f1549d956b618ed1a0f6939938
Surafel Melese <surafelmelese09@gmail.com>
2021-08-14 12:18:20 +0300

    3rd commit

--- bin/django-admin.py
+++ bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/sura/Documents/fresh_start/djangoenv/bin/python
+#!/home/sura/Documents/fresh_start/djangoenv/bin/python3
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
