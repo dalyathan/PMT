fc2672404924b6682e25e65a098ae7af9124662a
Surafel Melese <surafelmelese09@gmail.com>
2021-08-15 00:00:51 +0300

    some changes

--- backtools/settings.py
+++ backtools/settings.py
@@ -42,7 +42,7 @@ STATICFILES_DIRS = (
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-j08h-4fjkwrd6y5t&d!ti4adzw&5w(jhxks%vv48^w4%3v_^hj'#os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
