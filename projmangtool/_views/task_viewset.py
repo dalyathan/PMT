1b70f96c56953d5431edd110db84d0e0cd25e0fa
Surafel Melese <surafelmelese09@gmail.com>
2021-08-17 11:20:51 +0300

    manager could now approve task completion

--- projmangtool/_views/task_viewset.py
+++ projmangtool/_views/task_viewset.py
@@ -1,4 +1,5 @@
 import datetime
+from django.db.models.base import Model
 from rest_framework import viewsets
 from projmangtool import models,serializers
 from rest_framework import permissions
@@ -41,11 +42,35 @@ class TaskViewSet(viewsets.ModelViewSet):
             return Response('unexpected status id='+task['status']+' provided', status.HTTP_400_BAD_REQUEST)
         if not self.is_task_assigned_to_user(request):
             return Response('You are not assigned to this task', status.HTTP_400_BAD_REQUEST)
-        updated_task= models.Task.objects.get(pk= int(task['id']))
+        try:
+            updated_task= models.Task.objects.get(pk= int(task['id']))
+        except models.Task.DoesNotExist:
+             return Response('No such task', status.HTTP_400_BAD_REQUEST)
         updated_task.status= models.Task.TaskStatus.values[int(task['status'])]
         updated_task.save()
         self.updaterole(request)
         return Response('Task Status Updated', status.HTTP_200_OK)
+
+    @action(methods=['post'], detail= False)
+    def approvetask(self, request):
+        #task: int
+        #check whether this is the manager of the project
+        try:
+            task_from_db= models.Task.objects.get(pk= int(request.data['task']))
+        except models.Task.DoesNotExist:
+            return Response('No such task', status.HTTP_400_BAD_REQUEST)
+        if task_from_db.project.manager == request.user:
+            print(task_from_db.project.manager)
+            if task_from_db.status == models.Task.TaskStatus.REJECTED:
+                return Response('This task has been declined by the developer', status.HTTP_400_BAD_REQUEST)
+            elif task_from_db.status != models.Task.TaskStatus.SUBMITTED:
+                return Response('Task not completed yet', status.HTTP_400_BAD_REQUEST)
+            else:
+                task_from_db.status= models.Task.TaskStatus.COMPLETED
+                return Response('Task Status Updated', status.HTTP_200_OK)
+        else:
+            return Response('You dont have permission for this', status.HTTP_400_BAD_REQUEST)
+
     def updaterole(self, request):
         """Update role of developer if this is the only task in this project"""
         task_data= request.data
@@ -57,11 +82,13 @@ class TaskViewSet(viewsets.ModelViewSet):
             new_role= models.Role.objects.create(
                 user= request.user, role= models.Role.UserRole.DEVELOPER, project= task_from_db.project)
             new_role.save()
+    
     def is_task_assigned_to_user(self, request):
         """checks whether this task is assigned to this user"""
         task_data= request.data
         task_from_db= models.Task.objects.get(pk= int(task_data['id']))
         return task_from_db.dev == request.user
+    
     def haspermission(self, request):
         """Checks wether current user has permission to access this task"""
         pass
